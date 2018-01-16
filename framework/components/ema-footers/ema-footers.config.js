7b524f13e79ea955d905ce3c51631560b7bcd9c6
Romain Emery <emery.romain@gmail.com>
2018-01-16 13:30:38 +0100

    feat(footer): add component

---
+++ framework/components/ema-footers/ema-footers.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  title: 'Footers',
+  label: 'Footers',
+  status: 'ready',
+  tags: ['molecule'],
+  context: {
+    links: [
+      { label: 'Help', href: '#' },
+      { label: 'Sitemap', href: '#' },
+      { label: 'EMA public website', href: '#', variant: 'external' },
+    ],
+    copyright:
+      '&copy; 2016 EMA - 30 Churchill Place - Canary Whaf - London E14 5EU - United Kingdom',
+    description: 'An agency of the European Union',
+    logo: {
+      src: '/framework/images/eu.png',
+      alt: 'European Union flag',
+    },
+  },
+};
