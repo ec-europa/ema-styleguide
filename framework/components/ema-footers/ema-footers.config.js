9e019536ec7597f94ab7aa18d75fc68abfccb1bf
Romain Emery <emery.romain@gmail.com>
2018-01-16 14:41:37 +0100

    feat(footer): add component

--- framework/components/ema-footers/ema-footers.config.js
+++ framework/components/ema-footers/ema-footers.config.js
@@ -5,9 +5,13 @@ module.exports = {
   tags: ['molecule'],
   context: {
     links: [
-      { label: 'Help', href: '#' },
-      { label: 'Sitemap', href: '#' },
-      { label: 'EMA public website', href: '#', variant: 'external' },
+      { label: 'Help', href: '#', variant: ['inverted', 'standalone'] },
+      { label: 'Sitemap', href: '#', variant: ['inverted', 'standalone'] },
+      {
+        label: 'EMA public website',
+        href: '#',
+        variant: ['inverted', 'standalone', 'external'],
+      },
     ],
     copyright:
       '&copy; 2016 EMA - 30 Churchill Place - Canary Whaf - London E14 5EU - United Kingdom',
