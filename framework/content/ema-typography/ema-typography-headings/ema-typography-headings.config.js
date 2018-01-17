9f5a2fe6f19b16001249d0c51692d0e7b9e2a42d
Romain Emery <emery.romain@gmail.com>
2018-01-17 10:16:37 +0100

    refactor(modifiers): cleanup code

--- framework/content/ema-typography/ema-typography-headings/ema-typography-headings.config.js
+++ framework/content/ema-typography/ema-typography-headings/ema-typography-headings.config.js
@@ -2,4 +2,44 @@ module.exports = {
   title: 'Headings',
   label: 'Headings',
   status: 'ready',
+  collated: true,
+  default: 'h1',
+  variants: [
+    {
+      name: 'h1',
+      label: 'h1',
+      context: {
+        element: 'h1',
+        class: 'ecl-heading ecl-heading--ema-h1',
+        text: 'Heading <h1>: class ema-heading ema-heading--h1',
+      },
+    },
+    {
+      name: 'h2',
+      label: 'h2',
+      context: {
+        element: 'h2',
+        class: 'ecl-heading ecl-heading--ema-h2',
+        text: 'Heading <h2>: class ema-heading ema-heading--h2',
+      },
+    },
+    {
+      name: 'h3',
+      label: 'h3',
+      context: {
+        element: 'h3',
+        class: 'ecl-heading ecl-heading--ema-h3',
+        text: 'Heading <h3>: class ema-heading ema-heading--h3',
+      },
+    },
+    {
+      name: 'h4',
+      label: 'h4',
+      context: {
+        element: 'h4',
+        class: 'ecl-heading ecl-heading--ema-h4',
+        text: 'Heading <h4>: class ema-heading ema-heading--h4',
+      },
+    },
+  ],
 };
