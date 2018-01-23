fc14cf9ecd737229dc42cd81889750ccbfbe81af
Romain Emery <emery.romain@gmail.com>
2018-01-23 14:26:14 +0100

    quick fix: image path

--- framework/components/ema-footers/ema-footers.config.js
+++ framework/components/ema-footers/ema-footers.config.js
@@ -26,7 +26,7 @@ module.exports = {
       '&copy; 2016 EMA - 30 Churchill Place - Canary Whaf - London E14 5EU - United Kingdom',
     description: 'An agency of the European Union',
     logo: {
-      src: '/framework/images/eu.png',
+      src: '../../framework/images/eu.png',
       alt: 'European Union flag',
     },
   },
