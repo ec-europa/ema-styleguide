da85a498a899a53b315c3c393d258b97528ac294
Romain Emery <emery.romain@gmail.com>
2018-01-25 15:28:56 +0100

    feat(content-page): update template

--- framework/templates/ema-content-pages/ema-content-pages.config.js
+++ framework/templates/ema-content-pages/ema-content-pages.config.js
@@ -60,39 +60,40 @@ module.exports = {
     menu: {
       menu_label: 'Menu',
       menu_aria_label: 'Main Navigation',
+      extra_classes: 'ecl-navigation-menu--ema',
       links: [
         {
           label: 'Home',
           href: '#home',
-          is_active: true,
         },
         {
-          label: 'Item 1',
+          label: 'Human regulatory',
           href: '#item1',
+          title: 'Human regulatory',
+          description:
+            'This section of the website provides information on the regulation of medicines for human use in the European Union (EU). It particularly concerns the centralised procedure, where the European Medicines Agency (EMA) plays a key role.',
+          is_active: true,
           children_links: [
             {
-              label: 'Item 1.1',
+              label: 'Overview',
               href: '#item1-1',
             },
             {
-              label: 'Item 1.2',
-              href: '#item1-2',
-            },
-            {
-              label: 'Item 1.3',
-              href: '#item1-3',
-            },
-            {
-              label: 'Item 1.4',
+              label: 'Marketing authorisation',
               href: '#item1-4',
             },
             {
-              label: 'Item 1.5',
+              label: 'Research and development',
+              href: '#item1-2',
+              is_active: true,
+            },
+            {
+              label: 'Post authorisation',
               href: '#item1-5',
             },
             {
-              label: 'Item 1.6',
-              href: '#item1-6',
+              label: 'Herbal products',
+              href: '#item1-3',
             },
           ],
         },
@@ -112,7 +113,51 @@ module.exports = {
               label: 'Item 2.3',
               href: '#item2-3',
             },
+            {
+              label: 'Item 2.4',
+              href: '#item2-4',
+            },
+            {
+              label: 'Item 2.5',
+              href: '#item2-5',
+            },
+            {
+              label: 'Item 2.6',
+              href: '#item2-6',
+            },
           ],
+        },
+        {
+          label: 'Item 3',
+          href: '#item3',
+        },
+      ],
+    },
+    banner: {
+      title: 'Human regulatory',
+      description:
+        'This section of the website provides information on the regulation of medicines for human use in the European Union (EU). It particularly concerns the centralised procedure, where the European Medicines Agency (EMA) plays a key role.',
+      links: [
+        {
+          label: 'Overview',
+          href: '#item1-1',
+        },
+        {
+          label: 'Marketing authorisation',
+          href: '#item1-4',
+        },
+        {
+          label: 'Research and development',
+          href: '#item1-2',
+          is_active: true,
+        },
+        {
+          label: 'Post authorisation',
+          href: '#item1-5',
+        },
+        {
+          label: 'Herbal products',
+          href: '#item1-3',
         },
       ],
     },
