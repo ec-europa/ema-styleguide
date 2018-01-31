778cdbf056e1767ccaed2c5e7c1c138aa72942c7
Radu Chiriac <contact@raduchiriac.me>
2018-01-31 18:05:30 +0100

    Add two types of lists

--- framework/components/ema-content-slider/ema-content-slider.config.js
+++ framework/components/ema-content-slider/ema-content-slider.config.js
@@ -2,5 +2,40 @@ module.exports = {
   title: 'Content slider',
   label: 'Content slider',
   status: 'ready',
-  context: {},
+  context: {
+    list: [
+      {
+        title: 'Labore nulla quis irure labore.',
+        metas: ['data', 'cluster'],
+        primary_image: {
+          src: 'https://unsplash.it/640/480?image=222',
+          alt: 'sky',
+        },
+      },
+      {
+        metas: ['message', 'market'],
+        title: 'Velit mollit in sunt dolor exercitation elit nostrud do.',
+        primary_image: {
+          src: 'https://unsplash.it/640/480?image=223',
+          alt: 'street',
+        },
+      },
+      {
+        metas: ['content', 'dynamic'],
+        title: 'Cupidatat commodo reprehenderit excepteur esse.',
+        primary_image: {
+          src: 'https://unsplash.it/640/480?image=221',
+          alt: 'buildings',
+        },
+      },
+      {
+        metas: ['management', 'team'],
+        title: 'Excepteur ex dolore.',
+        primary_image: {
+          src: 'https://unsplash.it/640/480?image=225',
+          alt: 'tea',
+        },
+      },
+    ],
+  },
 };
