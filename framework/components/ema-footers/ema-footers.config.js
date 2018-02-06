52b3a569a7c5b91a8ba3eec0313a20321e923a33
Romain Emery <emery.romain@gmail.com>
2018-02-06 07:56:53 +0100

    refactor(styleguide): use latest version of EMA styleguide

--- framework/components/ema-footers/ema-footers.config.js
+++ framework/components/ema-footers/ema-footers.config.js
@@ -4,26 +4,136 @@ module.exports = {
   status: 'ready',
   tags: ['molecule'],
   context: {
-    links: [
+    sections: [
       {
-        label: 'Help',
-        href: '#',
-        extra_classes: 'ecl-link--ema-inverted ema-footer__link-item',
+        title: 'About',
+        links: [
+          {
+            label: 'Who we are',
+            href: '#',
+            extra_classes: 'ecl-link--ema-inverted ecl-link--ema-standalone',
+          },
+          {
+            label: "What's new on the site",
+            href: '#',
+            extra_classes: 'ecl-link--ema-inverted ecl-link--ema-standalone',
+          },
+          {
+            label: 'Expert blog',
+            href: '#',
+            extra_classes: 'ecl-link--ema-inverted ecl-link--ema-standalone',
+          },
+          {
+            label: 'Human regulation',
+            href: '#',
+            extra_classes: 'ecl-link--ema-inverted ecl-link--ema-standalone',
+          },
+          {
+            label: 'Veterinary regulation',
+            href: '#',
+            extra_classes: 'ecl-link--ema-inverted ecl-link--ema-standalone',
+          },
+          {
+            label: 'Commitees',
+            href: '#',
+            extra_classes: 'ecl-link--ema-inverted ecl-link--ema-standalone',
+          },
+        ],
       },
       {
-        label: 'Sitemap',
-        href: '#',
-        extra_classes: 'ecl-link--ema-inverted ema-footer__link-item',
+        title: 'Ask EMA',
+        links: [
+          {
+            label: 'Public enquiries',
+            href: '#',
+            extra_classes: 'ecl-link--ema-inverted ecl-link--ema-standalone',
+          },
+          {
+            label: 'FAQs',
+            href: '#',
+            extra_classes: 'ecl-link--ema-inverted ecl-link--ema-standalone',
+          },
+          {
+            label: 'Access to documents',
+            href: '#',
+            extra_classes: 'ecl-link--ema-inverted ecl-link--ema-standalone',
+          },
+        ],
       },
       {
-        label: 'EMA public website',
-        href: '#',
-        extra_classes:
-          'ecl-link--ema-inverted ecl-link--ema-external ema-footer__link-item',
+        title: 'Links',
+        links: [
+          {
+            label: 'Legal',
+            href: '#',
+            extra_classes: 'ecl-link--ema-inverted ecl-link--ema-standalone',
+          },
+          {
+            label: 'Privacy',
+            href: '#',
+            extra_classes: 'ecl-link--ema-inverted ecl-link--ema-standalone',
+          },
+          {
+            label: 'Complaints',
+            href: '#',
+            extra_classes: 'ecl-link--ema-inverted ecl-link--ema-standalone',
+          },
+          {
+            label: 'Browser compatibility',
+            href: '#',
+            extra_classes: 'ecl-link--ema-inverted ecl-link--ema-standalone',
+          },
+          {
+            label: 'Contacts',
+            href: '#',
+            extra_classes: 'ecl-link--ema-inverted ecl-link--ema-standalone',
+          },
+          {
+            label: 'Business hours and holidays',
+            href: '#',
+            extra_classes: 'ecl-link--ema-inverted ecl-link--ema-standalone',
+          },
+          {
+            label: 'Website help',
+            href: '#',
+            extra_classes: 'ecl-link--ema-inverted ecl-link--ema-standalone',
+          },
+        ],
+      },
+      {
+        title: 'Contact',
+        links: [
+          {
+            label:
+              'EMA<br />30 Churchill Place<br />Canary Whaf<br />London E14 5EU<br />United Kingdom',
+            href: '#',
+            extra_classes: 'ecl-link--ema-inverted ecl-link--ema-standalone',
+          },
+          {
+            label: 'Tel: +44 (0)20 3660 6000',
+            href: '#',
+            extra_classes: 'ecl-link--ema-inverted ecl-link--ema-standalone',
+          },
+          {
+            label: 'Fax: +44 (0)20 3660 5550',
+            href: '#',
+            extra_classes: 'ecl-link--ema-inverted ecl-link--ema-standalone',
+          },
+          {
+            label: 'Send a question',
+            href: '#',
+            extra_classes:
+              'ecl-link--ema-inverted ecl-link--ema-external ecl-link--ema-standalone',
+          },
+        ],
       },
     ],
-    copyright:
-      '&copy; 2016 EMA - 30 Churchill Place - Canary Whaf - London E14 5EU - United Kingdom',
+    hotline: {
+      href: '#',
+      title: 'Product emergency hotline',
+      description: 'outside working hours',
+    },
+    copyright: '&copy; 1995-2016 European Medicines Agency',
     description: 'An agency of the European Union',
     logo: {
       src: '../../framework/images/eu.png',
