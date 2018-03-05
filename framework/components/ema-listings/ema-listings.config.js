217a6bf27a16c0185e67095102a77009fdf78b4d
Radu Chiriac <contact@raduchiriac.me>
2018-03-05 11:10:12 +0100

    Add a font-weight normal style (#28)

--- framework/components/ema-listings/ema-listings.config.js
+++ framework/components/ema-listings/ema-listings.config.js
@@ -11,29 +11,29 @@ module.exports = {
     items: [
       {
         title:
-          'Checklist for sponsors applying for the transfer of orphan medicinal product designation <small class="ema-u-color-grey-2">(PDF - 230kb)</small>',
+          'Checklist for sponsors applying for the transfer of orphan medicinal product designation <small class="ema-u-color-grey-2 ecl-list-item__title--normal">(pdf - 230kb)</small>',
         detail:
           '<div class="ema-u-color-grey-2"><small>Published 15/01/2009<br />Updated 13/11/2017</small></div>',
         extra_classes: 'ecl-list-item--ema-pdf',
 
         translations: [
-          { title: 'Titre du fichier', meta: '230 kB - PDF - 4 pages' },
-          { title: 'Dateititel', meta: '230 kB - PDF - 4 pages' },
-          { title: 'Файл Заглавие', meta: '230 kB - PDF - 4 pages' },
+          { title: 'Titre du fichier', meta: '230 kB - pdf - 4 pages' },
+          { title: 'Dateititel', meta: '230 kB - pdf - 4 pages' },
+          { title: 'Файл Заглавие', meta: '230 kB - pdf - 4 pages' },
         ],
         translations_label: 'Available languages (3)',
         translations_tooltip: 'Click to see translations',
       },
       {
         title:
-          'Template 1 - Orphan designation transfer form <small class="ema-u-color-grey-2">(DOC - 512kb)</small>',
+          'Template 1 - Orphan designation transfer form <small class="ema-u-color-grey-2 ecl-list-item__title--normal">(doc - 512kb)</small>',
         detail:
           '<div class="ema-u-color-grey-2"><small>Published 13/04/2016<br />Updated 13/12/2016</small></div>',
         extra_classes: 'ecl-list-item--ema-doc',
       },
       {
         title:
-          'Template 2 - Translations of the active ingredient and indication for transfer of orphan-medicinal-product designation <small class="ema-u-color-grey-2">(DOCX - 132kb)</small>',
+          'Template 2 - Translations of the active ingredient and indication for transfer of orphan-medicinal-product designation <small class="ema-u-color-grey-2 ecl-list-item__title--normal">(docx - 132kb)</small>',
         detail:
           '<div class="ema-u-color-grey-2"><small>Published 24/11/2008<br />Updated 04/07/2013</small></div>',
         extra_classes: 'ecl-list-item--ema-doc',
