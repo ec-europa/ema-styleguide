3ccbdd983abb75f8c949862fff9a95b69c69f6e6
wesley deglise <wesley.deglise@gmail.com>
2018-03-06 08:39:58 +0100

    fix(icons): fix path to icons in ema-icons.config.js - no issue (#30)
    fix(icons): fix path to icons in ema-icons.config.js - no issue

--- framework/content/ema-icons/ema-icons.config.js
+++ framework/content/ema-icons/ema-icons.config.js
@@ -13,7 +13,7 @@ module.exports = {
   },
   default: 'doi',
   context: {
-    icon_path: '/framework/images/icons/ema-icons.svg',
+    icon_path: '../../framework/images/icons/ema-icons.svg',
   },
   variants: [
     {
