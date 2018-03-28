0eedb0df7fbbe0f73bd4df683c72f14bb20c8633
wesley deglise <wesley.deglise@gmail.com>
2018-03-28 11:04:07 +0200

    feat(status-referral): init component (#45)
    * feat(status-referral): init component
    
    * feat(status-referral): init component
    
    * fix(status referral):  bem typo

---
+++ framework/components/ema-status-referral/ema-status-referral.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  title: 'Status referral',
+  label: 'Status referral',
+  status: 'ready',
+  context: {
+    items: [
+      { title: 'Procedure started' },
+      { title: 'Under evaluation' },
+      { title: 'PRAC recommendation' },
+      { title: 'CHMP opinion', active: true },
+      { title: 'European Commission final decision' },
+    ],
+  },
+};
